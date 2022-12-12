b8be72d069142f0584116d8e6bc83b596688b074
MrBlack3110 <103769242+MrBlack3110@users.noreply.github.com>
2022-12-12 19:15:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <div className="h1">SHAASTRA 2023</div>
+        <div className="h3">Jan 26-29...</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
