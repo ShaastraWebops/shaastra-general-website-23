486af11a0e6572bb4d4c73d013a0b714f7c5f3d5
krithikaa22 <kuruthiga22@gmail.com>
2022-12-13 09:43:32 +0530

    logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import logo from "./23_white logo-02.png"
 
 function App() {
   return (
     <div className="App">
       <div className="content">
+      <img src={logo}></img>
         <div className="h1">SHAASTRA 2023</div>
         <div className="h3">Jan 26-29... Coming Soon</div>
       </div>
