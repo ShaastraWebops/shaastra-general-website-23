9996c415d085a7661ee61be5def121fdbcc78c40
thunder-volt <ishu114407@gmail.com>
2023-01-21 01:53:29 +0530

    shows/spotlight updated

---
+++ src/Components/Shows.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/ComingSoon.css"
+const Shows = () => {
+    return (
+        <div className='coming-body'>
+            <h2 className='headingComing'>Coming Soon</h2>
+        </div>
+
+    )
+}
+
+export default Shows
