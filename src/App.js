27f1358bf5227d08fb4ddf6599cebffa52dbd84a
Samarth KJ <unspec5d@gmail.com>
2023-01-24 21:28:28 +0530

    debunk added

--- src/App.js
+++ src/App.js
@@ -1,72 +1,40 @@
-import Login from "./components/login";
-import Signup from "./components/signup";
-import ForgotPassword from "./components/ForgotPassword";
-import ResetPassword from "./components/ResetPassword";
-import Verify from "./components/Verify";
-import Admin from "./components/Admin";
-import EditUser from "./components/EditUser";
-import AddEvent from "./components/AddEvent";
-import EditEvent from "./components/EditEvent";
-
+import Login from "./Components/login";
+import Signup from "./Components/signup";
+import ForgotPassword from "./Components/ForgotPassword";
+import ResetPassword from "./Components/ResetPassword";
+import Verify from "./Components/Verify";
+import Admin from "./Components/Admin";
+import EditUser from "./Components/EditUser";
+import AddEvent from "./Components/AddEvent";
+import EditEvent from "./Components/EditEvent";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import PayRegister from "./components/payRegister";
-import Exhibition from "./components/Exhibition";
-import Envisage from "./components/Envisage";
-import TIF from "./components/TIF";
-import Profile from "./components/Profile";
-import Events from "./components/EventsLanding";
-import EventsLanding from "./components/EventsLanding";
-import WorkshopsLanding0 from "./components/WorkshopsLanding";
-import Team from "./components/pages/Team_page/Team.tsx";
-import Spons from "./components/Spons";
-import Privacy from "./components/policies/Privacy";
-import Refund from "./components/policies/Refund";
-import Shipping from "./components/policies/Shipping";
-import Terms from "./components/policies/TermsConditions";
-import Vastra from "./components/vastra";
-import Homepage from "./components/Homepage";
-import ProfileEdit from "./components/ProfileEdit";
-import Workshop from "./components/Workshop";
-import WorkshopsLanding from "./components/WorkshopsLanding"
-import GetCSV from "./components/Getcsv";
-import ChessBlitz from "./blitz-chess";
-
-import Temp from "./components/tempHome";
+import PayRegister from "./Components/payRegister";
+import Exhibition from "./Components/Exhibition";
+import Envisage from "./Components/Envisage";
+import TIF from "./Components/TIF";
+import Profile from "./Components/Profile";
+import Events from "./Components/EventsLanding";
+import EventsLanding from "./Components/EventsLanding";
+import WorkshopsLanding from "./Components/WorkshopsLanding";
+import Team from "./Components/pages/Team_page/Team.tsx";
+import Spons from "./Components/Spons";
+import Privacy from "./Components/policies/Privacy";
+import Refund from "./Components/policies/Refund";
+import Shipping from "./Components/policies/Shipping";
+import Terms from "./Components/policies/TermsConditions";
+import Vastra from "./Components/vastra";
+import Homepage from "./Components/Homepage";
+import ProfileEdit from "./Components/ProfileEdit";
+import Workshop from "./Components/Workshop";
+import Debunk from "./Components/debunk"
+import Temp from "./Components/tempHome";
 
 import Contact from "./Contact";
-import React, { useState, useEffect } from "react";
-import ReactGA from 'react-ga';
-
-import { Modal2 } from "./components/Modal2";
-import { Container, ButtonModal } from "./components/modalContainer";
-import Spotlight from "./components/Spotlight";
-import Shows from "./components/Shows";
-
-const TRACKING_ID = "UA-220438183-5";
-ReactGA.initialize(TRACKING_ID);
 
 function App() {
-  const [auth, setAuth] = useState("");
-  const verifier = localStorage.getItem("role") || "";
-
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
-  useEffect(() => {
-    setAuth(localStorage.getItem("role"));
-    console.log(verifier);
-  }, [verifier]);
-  const [showModal2, setShowModal2] = useState(false);
-  const openModal2 = () => {
-    setShowModal2((prev) => !prev);
-  };
-
   return (
     <div>
-
-
       <Router>
         <Routes>
           <Route exact path="/" element={<Homepage />} />
@@ -83,32 +51,25 @@ function App() {
           <Route path="/workshops" element={<WorkshopsLanding />} />
           <Route path="/team" element={<Team />} />
           <Route path="/sponsors" element={<Spons />} />
-          <Route path="/spotlight" element={<Spotlight />} />
-          <Route path="/shows" element={<Shows />} />
           <Route path="/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/debunk" element={<Debunk />} />
           <Route
             path="/forgotpassword/resetpassword"
             element={<ResetPassword />}
           />
+          <Route path="/admin/workshops" element={<Workshop />} />
           <Route path="/verify" element={<Verify />} />
           <Route path="/policies/Privacy" element={<Privacy />} />
           <Route path="/policies/Shipping" element={<Shipping />} />
           <Route path="/policies/Terms" element={<Terms />} />
           <Route path="/policies/Refund" element={<Refund />} />
           <Route path="/verify" element={<Verify />} />
+          <Route path="/admin" element={<Admin />} />
           {/* <Route path="/pay/:id" element={<PayRegister />} /> */}
+          <Route path="/admin/addevent" element={<AddEvent />} />
+          <Route path="/admin/edit/:id" element={<EditEvent />} />
           <Route path="/socialendeavours" element={<Vastra />} />
-          <Route path="/blitzchess" element={<ChessBlitz />} />
         </Routes>
-        {auth === "admin" && (
-          <Routes>
-            <Route path="/getcsv" element={<GetCSV />} />
-            <Route path="/admin/workshops" element={<Workshop />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/admin/addevent" element={<AddEvent />} />
-            <Route path="/admin/edit/:id" element={<EditEvent />} />
-          </Routes>
-        )}
       </Router>
     </div>
   );
--- src/Contact.js
+++ src/Contact.js
@@ -1,34 +1,34 @@
 import './App.css';
-import {AiOutlineMail} from "react-icons/ai";
-import {BsTelephone} from "react-icons/bs";
-import TopBar from './components/TopBar';
-import NavBar from './components/navigation/NavBar';
-import Footer from './components/Footer';
+import { AiOutlineMail } from "react-icons/ai";
+import { BsTelephone } from "react-icons/bs";
+import TopBar from './Components/TopBar';
+import NavBar from './Components/navigation/NavBar';
+import Footer from './Components/Footer';
 function Contact() {
   return (
-   <body>
-    <TopBar />
-    <NavBar />
-     <div className="contact-uspage">
-  <div class="contact-us-heading">
-    Contact Us
-  </div>
-  <div class="container contact-container">
-    <div class="left-side">
-      <div class="icon-container">
-        <div class="icon-box">
-        <AiOutlineMail class="icon"/>
+    <body>
+      <TopBar />
+      <NavBar />
+      <div className="contact-uspage">
+        <div class="contact-us-heading">
+          Contact Us
         </div>
-      </div>
-      <div class="content-container email-align">
-        <div>
-          <a href="mailto:sponsorship@shaastra.org">sponsorship@shaastra.org ➜</a><br />
-          <a href="mailto:outreach@shaastra.org">outreach@shaastra.org ➜</a><br />
-          <a href="mailto:sec_cocur@smail.iitm.ac.in">sec_cocur@shaastra.org ➜</a>
-        </div>
-      </div>
-    </div>
-    {/* <div class="right-side">
+        <div class="container contact-container">
+          <div class="left-side">
+            <div class="icon-container">
+              <div class="icon-box">
+                <AiOutlineMail class="icon" />
+              </div>
+            </div>
+            <div class="content-container email-align">
+              <div>
+                <a href="mailto:sponsorship@shaastra.org">sponsorship@shaastra.org ➜</a><br />
+                <a href="mailto:outreach@shaastra.org">outreach@shaastra.org ➜</a><br />
+                <a href="mailto:sec_cocur@smail.iitm.ac.in">sec_cocur@shaastra.org ➜</a>
+              </div>
+            </div>
+          </div>
+          {/* <div class="right-side">
       <div class="icon-container">
         <div class="icon-box">
          <BsTelephone class="icon"/>
@@ -79,10 +79,10 @@ function Contact() {
         </div>
       </div>
     </div> */}
-  </div>
-    </div>
-    <Footer />
-   </body>
+        </div>
+      </div>
+      <Footer />
+    </body>
   );
 }
 
